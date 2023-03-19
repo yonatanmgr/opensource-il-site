954d89f34f5076492a6e8b84ae2b21a0211d1f22
yonatanmgr <yonatanmgr@gmail.com>
2023-03-20 00:26:53 +0200

    testing

---
+++ src/components/Project.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+type ProjectProps = {
+    name: string,
+    url: string,
+    image: string,
+    description: string
+}
+
+export default function Project(props: ProjectProps){
+    return <div className="projectBlock">
+        <Image className="projectPreview" src={props.image} alt={props.name} />
+        <div className="projectName">{props.name}</div>
+    </div>
+}
