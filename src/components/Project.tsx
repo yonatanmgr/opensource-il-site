36f4ef89a03ca1c224e1ca8d49b880878cd12b03
yonatanmgr <yonatanmgr@gmail.com>
2023-03-20 01:42:51 +0200

    test image

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,17 +1,23 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 type ProjectProps = {
-    name: string,
-    url: string,
-    image: string,
-    description: string
-}
+  name: string;
+  url: string;
+  image: string;
+  description: string;
+};
 
-export default function Project(props: ProjectProps){
-
-    
-    return <div className="projectBlock">
-        <Image width={320} height={160} className="projectPreview" src={props.image} alt={props.name} />
-        <div className="projectName">{props.name}</div>
+export default function Project(props: ProjectProps) {
+  return (
+    <div className="projectBlock">
+      <img
+        width={320}
+        height={160}
+        className="projectPreview"
+        src={props.image}
+        alt={props.name}
+      />
+      <div className="projectName">{props.name}</div>
     </div>
+  );
 }
