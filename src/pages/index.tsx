a7e5dd26057c99d6078a218ce749354acd6945a4
yonatanmgr <yonatanmgr@gmail.com>
2023-03-20 00:42:26 +0200

    testing 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,62 +7,77 @@ import React, { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+type ProjProps = {
+  name: string;
+  url: string;
+  desc: string;
+};
+
+type LangProps = {
+  language: string;
+  projects: ProjProps[];
+};
+
 export default function Home() {
-  const [currentProjects, setCurrentProjects] = useState<any>([]);
-
-  const regex = /^\s?#{3}([^#{3}]+?)\n([^]+?)(?=^\s?#{3}[^#{3}])/gm;
-  const getTitle = /(?<=#{3} ).*/gm;
-  const getLangs =
-    /(?:^|\n)## Projects by main language\s[^\n]*\n(.*?)(?=\n##?\s|$)/gs;
-  const getLinks = /(?<=\* ).*/gm;
-  const parseLinkReg = /\[(.+)\]\((.+)\) - (.+)/;
-  const cleanDescReg = /!\[(.+)\]\(.+\)/;
-
-  const cleanDesc = (desc: string) => {
-    return desc.replace(cleanDescReg, "");
-  };
-
-  const parseLink = (proj: string) => {
-    const [, name, url, desc] = proj.match(parseLinkReg) as Array<string>;
-
-    return {
-      name: name,
-      url: url,
-      desc: cleanDesc(desc).trim(),
-    };
-  };
-
-  const processLinks = (headerContent: string) => {
-    const contMatch = headerContent.match(getLinks);
-    const link = (contMatch as string[]).map((l) => parseLink(l));
-    return link;
-  };
-
-  const processHeader = (data: string) => {
-    const temp = data.match(regex);
-
-    const t = (temp as string[]).map((element) => ({
-      language: (element.match(getTitle) as string[])[0],
-      projects: processLinks(element),
-    }));
-  };
-
-  fetch(
-    "https://raw.githubusercontent.com/lirantal/awesome-opensource-israel/master/README.md"
-  )
-    .then((response) => response.text())
-    .then((data) => {
-      const langs = data.match(getLangs);
-      const results = processHeader((langs as string[])[0]);
-      setCurrentProjects(results);
-    });
+  const [data, setData] = useState<LangProps[]>([]);
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    console.log(currentProjects);
-    
+    setLoading(true);
+    fetch(
+      "https://raw.githubusercontent.com/lirantal/awesome-opensource-israel/master/README.md"
+    )
+      .then((res) => res.text())
+      .then((data) => {
+        const processHeader = (data: string) => {
+          const getTitle = /(?<=#{3} ).*/gm;
 
-  }, [currentProjects])
-  
+          const temp = data.match(
+            /^\s?#{3}([^#{3}]+?)\n([^]+?)(?=^\s?#{3}[^#{3}])/gm
+          );
+
+          const processLinks = (headerContent: string) => {
+            const parseLink = (proj: string) => {
+              const [, name, url, desc] = proj.match(
+                /\[(.+)\]\((.+)\) - (.+)/
+              ) as Array<string>;
+              const cleanDesc = (desc: string) => {
+                return desc.replace(/!\[(.+)\]\(.+\)/, "");
+              };
+              return {
+                name: name,
+                url: url,
+                desc: cleanDesc(desc).trim(),
+              };
+            };
+
+            const contMatch = headerContent.match(/(?<=\* ).*/gm);
+            const link = (contMatch as string[]).map((l) => parseLink(l));
+            return link;
+          };
+
+          const t = (temp as string[]).map((element) => ({
+            language: (element.match(getTitle) as string[])[0],
+            projects: processLinks(element),
+          }));
+
+          return t as LangProps[];
+        };
+
+        setLoading(false);
+        const langs = data.match(
+          /(?:^|\n)## Projects by main language\s[^\n]*\n(.*?)(?=\n##?\s|$)/gs
+        );
+        const results = processHeader((langs as string[])[0]);
+        setData(results);
+        console.log(results);
+        
+      });
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
+
   return (
     <>
       <Head>
