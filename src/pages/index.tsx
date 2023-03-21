9c677d6560aa052496e273e6c7bde324e16c9434
yonatanmgr <yonatanmgr@gmail.com>
2023-03-22 00:52:22 +0200

    test api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,7 @@ export default function Home() {
 
   useEffect(() => {
     setLoading(true);
+    
     fetch(
       "https://raw.githubusercontent.com/lirantal/awesome-opensource-israel/master/README.md"
     )
@@ -84,6 +85,49 @@ export default function Home() {
         console.log(results);
       });
   }, []);
+
+  useEffect(() => {
+    let headersList = {
+      "Accept": "*/*",
+      "Authorization": "Bearer " + process.env.github_read_token,
+      "Content-Type": "application/json"
+     }
+     
+     let gqlBody = {
+       query: `query ($repoOwner: String!, $repoName: String!) {
+       repository(owner: $repoOwner, name: $repoName) {
+         languages(first: 3, orderBy: {field: SIZE, direction: DESC}) {
+           totalSize
+           edges {
+             size
+             node {
+               name
+             }
+           }
+         },
+         openGraphImageUrl,
+         shortDescriptionHTML,
+         pushedAt,
+         upCase: object(expression: "master:README.md") {
+           ... on Blob {
+             text
+           }
+         }
+       }
+     }`,
+       variables: {"repoOwner":"yonatanmgr","repoName":"mathberet"}
+     }
+     
+     let bodyContent =  JSON.stringify(gqlBody);
+     
+     fetch("https://api.github.com/graphql", { 
+       method: "POST",
+       body: bodyContent,
+       headers: headersList
+     }).then(res => console.log(res.text()));
+          
+  }, [])
+  
 
   useEffect(() => {
     const columns = [
