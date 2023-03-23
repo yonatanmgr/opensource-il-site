4c0dd938ee4a019e08b046163b82e99e14f965d6
yonatanmgr <yonatanmgr@gmail.com>
2023-03-23 02:26:49 +0200

    testing new grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,79 +6,123 @@ import React, { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-type ProjProps = {
-  name: string;
-  url: string;
-  desc: string;
+type RepoProps = {
+  nameWithOwner: string;
+  languages: {
+    edges: {
+      size: number;
+      node: { name: string };
+    }[];
+  };
+  openGraphImageUrl: string;
+  pushedAt: string;
+  shortDescriptionHTML: string;
+  upcase?: { text: string } | null;
 };
 
-type LangProps = {
-  language: string;
-  projects: ProjProps[];
+type orgProps = {
+  repositories: {
+    nodes: RepoProps[];
+  };
+};
+
+export type RepoColumns = {
+  name: string;
+  owner: string;
+  description: string;
+  lastCommit: string;
 };
 
 export default function Home() {
-  const [data, setData] = useState<LangProps[]>([]);
+  const [data, setData] = useState<any[]>([]);
   const [isLoading, setLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch(
+  //     "https://raw.githubusercontent.com/lirantal/awesome-opensource-israel/master/README.md"
+  //   )
+  //     .then((res) => res.text())
+  //     .then((data) => {
+  //       const processHeader = (data: string) => {
+  //         const getTitle = /(?<=#{3} ).*/gm;
+
+  //         const temp = data.match(
+  //           /^\s?#{3}([^#{3}]+?)\n([^]+?)(?=^\s?#{3}[^#{3}])/gm
+  //         );
+
+  //         const processLinks = (headerContent: string) => {
+  //           const parseLink = (proj: string) => {
+  //             const res = proj.match(
+  //               /\[(.+)\]\((.+)\) - (.+)/
+  //             ) as RegExpMatchArray;
+
+  //             if (res) {
+  //               const [, name, url, desc] = res;
+  //               const cleanDesc = (desc: string) => {
+  //                 return desc.replace(/!\[(.+)\]\(.+\)/, "");
+  //               };
+  //               return {
+  //                 name: name,
+  //                 url: url,
+  //                 desc: cleanDesc(desc).trim(),
+  //               };
+  //             } else
+  //               return {
+  //                 name: "ERR",
+  //                 url: "ERR",
+  //                 desc: "ERR",
+  //               };
+  //           };
+
+  //           const contMatch = headerContent.match(/(?<=\* ).*/gm);
+  //           const link = (contMatch as string[]).map((l) => parseLink(l));
+  //           return link;
+  //         };
+
+  //         const t = (temp as string[]).map((element) => ({
+  //           language: (element.match(getTitle) as string[])[0],
+  //           projects: processLinks(element),
+  //         }));
+
+  //         return t as LangProps[];
+  //       };
+
+  //       setLoading(false);
+  //       const langs = data.match(
+  //         /(?:^|\n)## Projects by main language\s[^\n]*\n(.*?)(?=\n##?\s|$)/gs
+  //       );
+  //       const results = processHeader((langs as string[])[0]);
+  //       setData(results);
+  //       console.log(results);
+  //     });
+  // }, []);
 
   useEffect(() => {
     setLoading(true);
-    fetch(
-      "https://raw.githubusercontent.com/lirantal/awesome-opensource-israel/master/README.md"
-    )
-      .then((res) => res.text())
+
+    fetch("https://os-il-api.vercel.app/api/reposdb")
+      .then((res) => res.json())
       .then((data) => {
-        const processHeader = (data: string) => {
-          const getTitle = /(?<=#{3} ).*/gm;
+        setData(
+          (data as { repository: RepoProps }[]).map((proj) => {
+            const projectObject = proj.repository;
+            const projectOwner = projectObject.nameWithOwner.split("/")[0];
+            const projectName = projectObject.nameWithOwner.split("/")[1];
+            const projectImage = projectObject.openGraphImageUrl;
+            const projectDescription = projectObject.shortDescriptionHTML;
+            const projectLastCommit = projectObject.pushedAt;
 
-          const temp = data.match(
-            /^\s?#{3}([^#{3}]+?)\n([^]+?)(?=^\s?#{3}[^#{3}])/gm
-          );
-
-          const processLinks = (headerContent: string) => {
-            const parseLink = (proj: string) => {
-              const res = proj.match(
-                /\[(.+)\]\((.+)\) - (.+)/
-              ) as RegExpMatchArray;
-
-              if (res) {
-                const [, name, url, desc] = res;
-                const cleanDesc = (desc: string) => {
-                  return desc.replace(/!\[(.+)\]\(.+\)/, "");
-                };
-                return {
-                  name: name,
-                  url: url,
-                  desc: cleanDesc(desc).trim(),
-                };
-              } else
-                return {
-                  name: "ERR",
-                  url: "ERR",
-                  desc: "ERR",
-                };
+            return {
+              image: projectImage,
+              owner: projectOwner,
+              name: projectName,
+              description: projectDescription,
+              lastCommit: projectLastCommit,
             };
-
-            const contMatch = headerContent.match(/(?<=\* ).*/gm);
-            const link = (contMatch as string[]).map((l) => parseLink(l));
-            return link;
-          };
-
-          const t = (temp as string[]).map((element) => ({
-            language: (element.match(getTitle) as string[])[0],
-            projects: processLinks(element),
-          }));
-
-          return t as LangProps[];
-        };
-
-        setLoading(false);
-        const langs = data.match(
-          /(?:^|\n)## Projects by main language\s[^\n]*\n(.*?)(?=\n##?\s|$)/gs
+          })
         );
-        const results = processHeader((langs as string[])[0]);
-        setData(results);
-        console.log(results);
+        setLoading(false)
       });
   }, []);
 
@@ -138,23 +182,16 @@ export default function Home() {
             </a>
           </div>
         </div>
-        <div className="langsList">
-          {data.map((lang) => {
+        <div className="projectGrid">
+          {data.map((proj) => {
             return (
-              <div key={lang.language} className="language">
-                <h1>{lang.language}</h1>
-                <div className="projectGrid">
-                  {lang.projects.map((proj) => (
-                    <Project
-                      image="https://assets-global.website-files.com/6207f5adfd8e615d3d70498b/6261753e28b7772bcf0266a0_Github%20comp.png"
-                      name={proj.name}
-                      description={proj.desc}
-                      url={proj.url}
-                      key={proj.url}
-                    />
-                  ))}
-                </div>
-              </div>
+              <Project
+                image={proj.image}
+                name={proj.name}
+                description={proj.description}
+                url={`https://www.github.com/${proj.owner}/${proj.name}`}
+                key={proj.image}
+              />
             );
           })}
         </div>
