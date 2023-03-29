d58ef5fb5b0d50caeb00071bee7f8f6cd9f924b6
yonatanmgr <yonatanmgr@gmail.com>
2023-03-29 17:50:55 +0300

    better looking filters

--- src/components/Header/Filters/FilterButton.tsx
+++ src/components/Header/Filters/FilterButton.tsx
@@ -1,9 +1,9 @@
-import { sortButtonsTexts } from "../constants";
-import { SortTypes } from "../types";
+// import { sortButtonsTexts } from "../constants";
+// import { SortTypes } from "../types";
 
 interface FilterButtonProps {
   onClick: () => void;
-  text: typeof sortButtonsTexts[SortTypes];
+  text: string;
   isActive: boolean;
 }
 
@@ -14,9 +14,8 @@ export const FilterButton = ({
 }: FilterButtonProps) => {
   return (
     <button
-      className={`h-8 text-sm hover:cursor-default focus-visible:ring-2 font-['Rubik'] rounded-xl px-4 bg-mydarkblue border-none outline outline-1 outline-myblue transition hover:bg-buttonhover  text-white ${
-        //can add class to give indication for active sort
-        isActive ? "" : ""
+      className={` cursor-default focus-visible:ring-2 w-12 sm:w-8 h-full font-['Rubik'] px-1 border-x border-myblue hover:text-white/90 transition active:text-sm ${
+        isActive ? "text-white" : "text-white/70"
       }`}
       onClick={onClick}
     >
