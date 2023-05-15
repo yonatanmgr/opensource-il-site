453a7a0957cc8a8177da83a1f0d1658fd2924875
darkmift <darkmift@gmail.com>
2023-05-15 20:49:47 +0300

    [main] added redis check

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -2,13 +2,15 @@ import { NextResponse } from 'next/server';
 // utils
 import { logger } from '@/server/utils/logger';
 import getUuid from '@/server/utils/uuid';
+import { getRedisVal } from '@/server/db/redis-vercel-kv';
 
 export async function GET() {
   const data = {
     requestId: getUuid(),
     uptime: process.uptime(),
     message: 'Ok',
-    date: new Date()
+    date: new Date(),
+    redis: await getRedisVal('test')
   };
   logger.info(data);
   return NextResponse.json(data);
