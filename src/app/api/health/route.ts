f4fa060217250ffc061ee9a741a9bf68034d74a4
darkmift <darkmift@gmail.com>
2023-05-16 22:31:11 +0300

    [rewrite-app-redis] added more test

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -2,17 +2,20 @@ import { NextResponse } from 'next/server';
 // utils
 import { logger } from '@/server/utils/logger';
 import getUuid from '@/server/utils/uuid';
-import { getRedisVal } from '@/server/db/redis-vercel-kv';
+import { getRedisVal, setRedisVal } from '@/server/db/redis-vercel-kv';
+import { NODE_ENV } from '@/server/config';
 
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
+  await setRedisVal('test', getUuid());
+
   const data = {
     requestId: getUuid(),
     uptime: process.uptime(),
     message: 'Ok',
     date: new Date(),
-    apiRedis: process.env.KV_REST_API_URL,
+    apiRedis: NODE_ENV,
     redis: await getRedisVal('test')
   };
   logger.info('/api/health', data);
