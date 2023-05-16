29f5f4fa1df8633eae6b1abffb82976045c8cfb1
darkmift <darkmift@gmail.com>
2023-05-16 21:03:10 +0300

    [rewrite-app-redis] minor bugfix/extra validation

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,4 @@
+import { getRedisVal, setRedisVal } from '@/server/db/redis-vercel-kv';
 import { mainDataFetch } from '@/server/services/dataManager.service';
 import { logger } from '@/server/utils/logger';
 import { NextResponse } from 'next/server';
@@ -6,6 +7,12 @@ export const dynamic = 'force-dynamic';
 
 export async function GET() {
   const data = await mainDataFetch();
-  logger.info('api/', { success: data?.success, date: new Date() });
+  await setRedisVal('test', new Date().toISOString());
+  const r = await getRedisVal('test');
+  logger.info('api/', {
+    successRedis: !!r,
+    dataSuccess: data?.success,
+    date: new Date()
+  });
   return NextResponse.json({ api: 'online', data });
 }
