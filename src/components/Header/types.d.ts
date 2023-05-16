af124beeff69b17b7f1df9f88623e2652605420a
yonatanmgr <yonatanmgr@gmail.com>
2023-05-17 00:13:08 +0300

    fix conflicts

--- src/components/Header/types.d.ts
+++ src/components/Header/types.d.ts
@@ -2,8 +2,6 @@ import { sortButtonsTexts } from './constants';
 
 export type SortTypes = keyof typeof sortButtonsTexts;
 export type AllSortTypes =
-  (typeof sortButtonsTexts)[keyof typeof sortButtonsTexts]["buttons"][number]["action"];
-export type AllSortTypes =
   (typeof sortButtonsTexts)[keyof typeof sortButtonsTexts]['buttons'][number]['action'];
 export interface TitleAndSocialLinkProps {
   setView: (view: Views) => void;
